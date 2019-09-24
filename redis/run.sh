8a7b65f87677530c7b9794db18026739752b267f
fusikai <fusikai@douyu.tv>
2019-09-24 12:37:02 +0800

    save

--- redis/run.sh
+++ redis/run.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-ports=("7000" "7001" "7002")
-for port in ${ports[@]}; do
-    echo $port
+redisserverports=("7000" "7001" "7002")
+for port in ${redisserverports[@]}; do
+    echo "start redis server $port"
     mkdir -p /root/data/$port
     redis-server redis-$port.conf
 done
+
+redissentinelports=("26379" "26380" "26381")
+for port in ${redissentinelports[@]}; do
+    echo "start redis sentinel $port"
+    redis-sentinel redis-sentinel-$port.conf
+done
+
 
